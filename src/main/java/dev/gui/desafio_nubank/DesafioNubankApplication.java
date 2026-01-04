74d6873a1293d3ca612c6cc0647feebb605d45fb
Guilherme Freires <guilhermefreires10@gmail.com>
2026-01-04 10:24:55 -0300

    Primeiro commit - projeto Java

---
+++ src/main/java/dev/gui/desafio_nubank/DesafioNubankApplication.java
@@ -0,0 +1,13 @@
+package dev.gui.desafio_nubank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioNubankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioNubankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gui/desafio_nubank/DesafioNubankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gui.desafio_nubank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioNubankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
